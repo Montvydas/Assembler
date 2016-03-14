a81987f5899b724f2126acac36f75a5ea882b00e
Montvydas Klumbys <monte@Montvydass-MacBook-Pro.local>
2016-03-14 14:44:58 +0000

    added functions to specify where mouse & timer isr start

--- translator/src/TranslatorMain.java
+++ translator/src/TranslatorMain.java
@@ -18,6 +18,9 @@ public class TranslatorMain {
 	static int lineNr = 0;
 	static String data = new String ();
 	static boolean containsErrors = false;
+    static String addressOfTimerISR = "11111110";
+    static String addressOfMouseISR = "11111111";
+    
     public static void main(String args[]) {
         try {
         	
@@ -31,6 +34,16 @@ public class TranslatorMain {
             FileWriter fw = new FileWriter(outputFile);
             
             boolean blockCommented = false;
+            int instructionAddress = 0;
+            String instructionAddressInHex = "00";
+            
+            
+            
+            
+            
+            
+            
+      //Start line scanning
             while (sc.hasNextLine()){	
             	lineNr++;
             	
@@ -77,7 +90,8 @@ public class TranslatorMain {
                 // System.out.println ("Length is " + storedCode.length);  
                 
                 String valueToSend = new String();
-                
+                int instructionLengthInBytes = 0;
+               
                 switch (storedCode[0].toUpperCase()){
                 	//load a 43
                 	//load b 33
@@ -85,6 +99,7 @@ public class TranslatorMain {
                         checkNumberOfOperands (storedCode.length, 3);
                     	valueToSend = checkWhichOperand (storedCode[1], "00000000", "00000001");
                     	valueToSend += hexToBi(storedCode[2]) + "\n";
+                        instructionLengthInBytes = 2;
                     break;
                     
                     //store 32 a
@@ -93,48 +108,56 @@ public class TranslatorMain {
                         checkNumberOfOperands (storedCode.length, 3);
                     	valueToSend =  checkWhichOperand (storedCode[2], "00000010", "00000011");
                     	valueToSend += hexToBi(storedCode[1]) + "\n";
+                        instructionLengthInBytes = 2;
                     break;
                     
                     //breq 52
                     case "BREQ":
                         checkNumberOfOperands (storedCode.length, 2);
                     	valueToSend = ("10010110" + '\n' + hexToBi(storedCode[1]) + '\n');
+                        instructionLengthInBytes = 2;
                     break;
                     
                     //bgtq 35
                     case "BGTQ":
                         checkNumberOfOperands (storedCode.length, 2);
                     	valueToSend = ("10100110" + '\n' + hexToBi(storedCode[1]) + '\n');
+                        instructionLengthInBytes = 2;
                     break;
                     
                     //bltq 45
                     case "BLTQ":
                         checkNumberOfOperands (storedCode.length, 2);
                     	valueToSend = ("10110110" + '\n' + hexToBi(storedCode[1]) + '\n');
+                        instructionLengthInBytes = 2;
                     break;  
                      
                     //goto_idle
                     case "GOTO_IDLE":
                         checkNumberOfOperands (storedCode.length, 1);
                     	valueToSend = ("00001000" + '\n');
+                        instructionLengthInBytes = 1;
                     break;
                     
                     //goto 43
                     case "GOTO":
                         checkNumberOfOperands (storedCode.length, 2);
                     	valueToSend = ("00000111" + '\n' + hexToBi(storedCode[1]) + '\n');
+                        instructionLengthInBytes = 2;
                     break;
                     
                     //call 35
                     case "CALL":
                         checkNumberOfOperands (storedCode.length, 2);
                     	valueToSend = ("00001001" + '\n' + hexToBi(storedCode[1]) + '\n');
+                        instructionLengthInBytes = 2;
                     break;
                     
                     //return
                     case "RETURN":
                         checkNumberOfOperands (storedCode.length, 1);
                     	valueToSend = ("00001010" + '\n');
+                        instructionLengthInBytes = 1;
                     break; 
                     
                     //deref a
@@ -142,6 +165,7 @@ public class TranslatorMain {
                     case "DEREF":
                         checkNumberOfOperands (storedCode.length, 2);
                     	valueToSend = checkWhichOperand (storedCode[1], "00001011", "00001100");
+                        instructionLengthInBytes = 1;
                     break;
                     
                     //add a b	//adds both and stores in a...
@@ -149,6 +173,7 @@ public class TranslatorMain {
                     case "ADD":
                         checkNumberOfOperands (storedCode.length, 3);
                     	valueToSend = checkOperandOrder (storedCode[1], storedCode[2], "00000100", "00000101");
+                        instructionLengthInBytes = 1;
                     break;
                     
                     //sub a b
@@ -156,6 +181,7 @@ public class TranslatorMain {
                     case "SUB":
                         checkNumberOfOperands (storedCode.length, 3);
                     	valueToSend = checkOperandOrder (storedCode[1], storedCode[2], "00010100", "00010101");
+                        instructionLengthInBytes = 1;
                     break; 
                     
                     //mult a b
@@ -163,6 +189,7 @@ public class TranslatorMain {
                     case "MULT":
                         checkNumberOfOperands (storedCode.length, 3);
                     	valueToSend = checkOperandOrder (storedCode[1], storedCode[2], "00100100", "00100101");
+                        instructionLengthInBytes = 1;
                     break;
                     
                     //s_l a
@@ -170,6 +197,7 @@ public class TranslatorMain {
                     case "S_L":
                         checkNumberOfOperands (storedCode.length, 2);
                     	valueToSend = checkWhichOperand (storedCode[1], "00110100", "00110101");
+                        instructionLengthInBytes = 1;
                     break;
                     
                     //s_r a
@@ -177,6 +205,7 @@ public class TranslatorMain {
                     case "S_R":
                         checkNumberOfOperands (storedCode.length, 2);
                     	valueToSend = checkWhichOperand (storedCode[1], "01000100", "01000101");
+                        instructionLengthInBytes = 1;
                     break;
                     
                     //incr a a	//incremends a and stores in a
@@ -188,6 +217,7 @@ public class TranslatorMain {
                     	else if (storedCode[1].equalsIgnoreCase("B"))
                     		valueToSend = checkWhichOperand (storedCode[2], "01010101", "01100101");
                     	else printErrorLine();
+                        instructionLengthInBytes = 1;
                     break; 
                     
                     //decr a b
@@ -199,6 +229,7 @@ public class TranslatorMain {
                     	else if (storedCode[1].equalsIgnoreCase("B"))
                     		valueToSend = checkWhichOperand (storedCode[2], "01110101", "10000101");
                     	else printErrorLine();
+                        instructionLengthInBytes = 1;
                     break;
                     
                     //equals a b
@@ -206,12 +237,14 @@ public class TranslatorMain {
                     case "EQUALS":
                         checkNumberOfOperands (storedCode.length, 3);
                     	valueToSend = checkOperandOrder (storedCode[1], storedCode[2], "10010100", "10010101");
+                        instructionLengthInBytes = 1;
                     break;
                     //greater a b
                     //greater b a
                     case "GREATER":
                         checkNumberOfOperands (storedCode.length, 3);
                     	valueToSend = checkOperandOrder (storedCode[1], storedCode[2], "10100100", "10100101");
+                        instructionLengthInBytes = 1;
                     break;
                     
                     //less a b
@@ -219,6 +252,7 @@ public class TranslatorMain {
                     case "LESS":
                         checkNumberOfOperands (storedCode.length, 3);
                     	valueToSend = checkOperandOrder (storedCode[1], storedCode[2], "10110100", "10110101");
+                        instructionLengthInBytes = 1;
                     break;
                     
                     //load_val a 32
@@ -227,7 +261,24 @@ public class TranslatorMain {
                     case "LOAD_VAL":
                         checkNumberOfOperands (storedCode.length, 3);
                         valueToSend = checkWhichOperand (storedCode[1], "00001101", "00001110");
-                        valueToSend += hexToBi(storedCode[2]);
+                        valueToSend += hexToBi(storedCode[2]) + "\n";
+                        instructionLengthInBytes = 2;
+                    break;
+                    
+                    //also check if it was used only once in our program
+                    //timer_isr:
+                    case "TIMER_ISR:":
+                        checkNumberOfOperands (storedCode.length, 1);
+                        // System.out.print ("\n" + instructionAddressInHex + "\n");
+                        addressOfTimerISR = hexToBi( instructionAddressInHex ) + "\n";
+                        instructionLengthInBytes = 0;
+                    break;
+                    
+                    //mouse_isr:
+                    case "MOUSE_ISR:":
+                        checkNumberOfOperands (storedCode.length, 1);
+                        addressOfMouseISR = hexToBi( instructionAddressInHex ) + "\n";
+                        instructionLengthInBytes = 0;
                     break;
                     
                     default:  printErrorLine();
@@ -236,6 +287,11 @@ public class TranslatorMain {
                 }
                 
                 fw.write(valueToSend);
+                instructionAddress += instructionLengthInBytes; //gets the address
+                instructionAddressInHex = Integer.toHexString( instructionAddress );    //good to know hex value
+                if (instructionAddressInHex.length() == 1){                             //but need to make sure that it has two values as hex, i.e. C -> 0C
+                    instructionAddressInHex = "0" + instructionAddressInHex;
+                }
                 
                 if (containsErrors){
                 	fw.write("Please fix your errors firstly.");
@@ -244,8 +300,29 @@ public class TranslatorMain {
                 	return;
                 }
                 
-                System.out.print("\n" + valueToSend + "\n");
+                System.out.print( "\n" + valueToSend + "XXXXXXXX <- This Instruction address is " + instructionAddressInHex + "\n\n");
             }
+            
+            if (addressOfTimerISR == "11111110"){ //0xFE
+                // printErrorLine();
+                System.err.print ("Error: You didn't specify where your timer interrupt begins.\n");
+                return;
+            }   
+            
+            
+            if (instructionAddress < 254){
+                while (instructionAddress < 253) {
+                    fw.write("\n");
+                    instructionAddress ++;    
+                }
+                fw.write (addressOfTimerISR);
+                fw.write (addressOfMouseISR);
+            } else {
+                printErrorLine();
+                System.err.print("hint: you used more than allowed 253 Bytes of program ROM.\n");
+                return;
+            }
+            
 //     close stuff
             fw.close();
             sc.close();
@@ -253,7 +330,9 @@ public class TranslatorMain {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
+        if (addressOfMouseISR == "11111111")
+            System.out.println ("Warning: You didn't use mouse interrupt anywhere.\n");
+            
         System.out.println("Finished without errors!");
         return ;
     }
@@ -296,6 +375,13 @@ public class TranslatorMain {
             return;
         }
    }
+   
+   public static void checkLengthOfHexAndBin (int binLength, int hexLength){
+       if (binLength != 8 || hexLength != 2){
+           printErrorLine();
+           System.err.print ("hint: entered value is a not valid hex value.\n");
+       }
+   }
     /**
      * Translate a given hexadecimal number to a binary sequence.
      *
@@ -306,8 +392,7 @@ public class TranslatorMain {
         String bi = "";
         for (int i = 0; i < hex_str.length(); i++)
             bi = bi + hexToBi_per_char(hex_str.charAt(i));
-        // if (bi.length() > 8)
-           // return "error: incorrect address.";
+        checkLengthOfHexAndBin(bi.length(), hex_str.length());
         return bi;
     }
 
@@ -317,7 +402,20 @@ public class TranslatorMain {
      * @param hex       the digit
      * @return          the corresponding binary number
      */
+     
+     public static String hex_To_Bi (String hex){
+        int i = Integer.parseInt(hex, 16);
+        String bin = Integer.toBinaryString(i);
+        
+        System.out.print ("In hexToBi: hex=" + hex + " bin=" + bin + "\n");
+        
+        // checkLengthOfHexAndBin(bin.length(), hex.length());
+        return bin;
+     }
+    
+     
     public static String hexToBi_per_char(char hex) {
+        
         if (hex == '0')
             return "0000";
         else if (hex == '1')
@@ -338,17 +436,17 @@ public class TranslatorMain {
             return "1000";
         else if (hex == '9')
             return "1001";
-        else if (hex == 'A')
+        else if (hex == 'A' || hex == 'a')
             return "1010";
-        else if (hex == 'B')
+        else if (hex == 'B' || hex == 'b')
             return "1011";
-        else if (hex == 'C')
+        else if (hex == 'C' || hex == 'c')
             return "1100";
-        else if (hex == 'D')
+        else if (hex == 'D' || hex == 'd')
             return "1101";
-        else if (hex == 'E')
+        else if (hex == 'E' || hex == 'e')
             return "1110";
-        else if (hex == 'F')
+        else if (hex == 'F' || hex == 'f')
             return "1111";
         else
             return "";
