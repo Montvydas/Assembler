90b237c30019f070f0fcb1419c6830c293ae96cc
Theo Scott <tmscott10@hotmail.co.uk>
2016-03-15 06:26:24 +0000

    added assembler, rom and ram

---
+++ translator/assembler.py
@@ -0,0 +1,191 @@
+# Assembler, Theo Scott s1231174
+import sys
+
+src_file = sys.argv[1]
+out_file = sys.argv[2]
+
+default_fill = "F"
+timer_interrupt_addr = "timer_interrupt"
+
+line_count = -1 # account for line nos starting at 1, not 0
+labels = {}
+
+# write functions
+def empty(fill, number_command):
+    out.write(fill + str(number_command) + "\n")
+
+def labelAddr(addr):
+    out.write(addr + "\n")
+
+def alu(op, reg):
+    id = "4" if reg == "A" else "5"
+    out.write(op + id + "\n")
+
+def memLine(fill, cmd, mem):
+    out.write(fill + str(cmd) + "\n")
+    out.write(mem + "\n")
+
+# first pass for labels
+with open(src_file, 'r') as src:
+    with open(out_file, 'w') as out:
+        for line in src:
+            line_count += 1
+            line = line.strip()
+            tokens = line.split()
+            # first check for comments, i.e. starting '#'
+            if "#" in tokens: # if comment in line, delete it and following tokens
+                idx = tokens.index("#")
+                # print str(-1*(len(tokens)-idx))
+                del tokens[-1*(len(tokens)-idx):]
+                line_count -= 1
+            # check for blank lines or comment lines after deletion
+            # print tokens
+            if len(tokens) == 0:
+                inst = ""
+            else:
+                inst = tokens[0]
+            # next check if line is a label
+            if(':' in line and '#' not in line):
+                label = tokens[0][:-1] # label is token without colon
+                labels[label] = hex(line_count)[2:] # value of label is its line in hex without '0h'
+                line_count -= 1 # don't count label as an instruction
+            elif inst == "LOAD":
+                line_count += 1 # LD/ST need extra byte
+            elif inst == "STORE":
+                line_count += 1
+            elif inst == "ADD":
+                pass
+            elif inst == "SUB":
+                pass
+            elif inst == "MUL":
+                pass
+            elif inst == "SHIFTL":
+                pass
+            elif inst == "SHIFTR":
+                pass
+            elif inst == "INCA":
+                pass
+            elif inst == "INCB":
+                pass
+            elif inst == "DECA":
+                pass
+            elif inst == "DECB":
+                pass
+            elif inst == "AND":
+                pass
+            elif inst == "OR":
+                pass
+            elif inst == "XOR":
+                pass
+            elif inst == "BREQ":
+                line_count += 1
+            elif inst == "BGTE":
+                line_count += 1
+            elif inst == "BLTE":
+                line_count += 1
+            elif inst == "GOTO":
+                line_count += 1
+            elif inst == "GOTO_IDLE":
+                pass
+            elif inst == "FUNC":
+                line_count += 1
+            elif inst == "RETURN":
+                pass
+            elif inst == "DEREF":
+                pass
+            else:
+                pass
+
+        # return to start of file now that labels and lines have been counted
+        line_count = -1
+        src.seek(0)
+
+        # now translate instructions
+        for line in src:
+            line_count += 1
+            line = line.strip()
+            tokens = line.split()
+            if len(tokens) == 0:
+                line_count -= 1
+                inst = ""
+            else:
+                inst = tokens[0]
+            # first check for comments, i.e. starting '#'
+            if "#" in tokens: # if comment in line, delete it and following tokens
+                idx = tokens.index("#")
+                del tokens[-1*(len(tokens)-idx):]
+                line_count -= 1
+            # next check if line is a label
+            if ':' in line and '#' not in line:
+                line_count -= 1 # don't count label as an instruction
+            elif inst == "LOAD":
+                line_count += 1 # LD/ST need extra byte
+                reg = 0 if tokens[1] == "A" else 1
+                memLine(default_fill, reg, tokens[2])
+            elif inst == "STORE":
+                line_count += 1
+                reg = 2 if tokens[1] == "A" else 3
+                memLine(default_fill, reg, tokens[2])
+            elif inst == "ADD":
+                alu("0", tokens[1])
+            elif inst == "SUB":
+                alu("1", tokens[1])
+            elif inst == "MUL":
+                alu("2", tokens[1])
+            elif inst == "SHIFTL":
+                alu("3", tokens[1])
+            elif inst == "SHIFTR":
+                alu("4", tokens[1])
+            elif inst == "INCA":
+                alu("5", tokens[1])
+            elif inst == "INCB":
+                alu("6", tokens[1])
+            elif inst == "DECA":
+                alu("7", tokens[1])
+            elif inst == "DECB":
+                alu("8", tokens[1])
+            elif inst == "AND":
+                alu("C", tokens[1])
+            elif inst == "OR":
+                alu("D", tokens[1])
+            elif inst == "XOR":
+                alu("E", tokens[1])
+            elif inst == "BREQ":
+                line_count += 1
+                label = tokens[1]
+                memLine("B", 6, labels[label])
+            elif inst == "BGTE":
+                line_count += 1
+                label = tokens[1]
+                memLine("B", 6, labels[label])
+            elif inst == "BLTE":
+                line_count += 1
+                label = tokens[1]
+                memLine("B", 6, labels[label])
+            elif inst == "GOTO":
+                line_count += 1
+                label = tokens[1]
+                memLine(default_fill, 7, labels[label])
+            elif inst == "GOTO_IDLE":
+                empty(default_fill, 8)
+            elif inst == "FUNC":
+                line_count += 1
+                label = tokens[1]
+                memLine(default_fill, 9, labels[label])
+            elif inst == "RETURN":
+                empty(default_fill)
+            elif inst == "DEREF":
+                deref = "B" if tokens[1] == "A" else "C"
+            elif inst == "":
+                pass
+            elif inst == "#":
+                pass
+            else:
+                print "INVALID COMMAND: [" + str(line_count) + "] " + line
+
+        while line_count < 253:
+            empty(default_fill, default_fill)
+            line_count += 1
+
+        if timer_interrupt_addr in labels:
+            labelAddr(labels[timer_interrupt_addr])
--- translator/script.asm
+++ translator/script.asm
@@ -1,31 +1,31 @@
-// Test program, Theo Scott s1231174
+# Test program, Theo Scott s1231174
 
 end_check:
-  LOAD A 0C             // load FF into A
-  LOAD B 0D             // load val of 0D into B
-  BREQ end              // end if 0D == FF
+  LOAD A 0C             # load FF into A
+  LOAD B 0D             # load val of 0D into B
+  BREQ end              # end if 0D == FF
 
 timer_interrupt:
-  LOAD A 08             // load val of 08 into A
-  INCR A A              // increment
-  STORE 08 A            // store in 08
-  LOAD B 09             // load 0A into B
-  BREQ ten_interrupts   // go to ten_interrupts if A == 0A
+  LOAD A 08             # load val of 08 into A
+  INCA A                # increment
+  STORE A 08            # store in 08
+  LOAD B 09             # load 0A into B
+  BREQ ten_interrupts   # go to ten_interrupts if A == 0A
 GOTO_IDLE
 
 ten_interrupts:
-  LOAD A 0B             // load 00 into A
-  STORE 08 A            // store 00 into 08
-  LOAD A 00             // load PC into A
-  INCR A A              // increment
-  STORE 00 A            // store new PC in 00
-  DEREF A               // deref register, now has instruction
-  STORE 90 A            // write inst to address 90
-  LOAD B 06             // load last inst of program
-  BREQ end              // if last inst, end program
+  LOAD A 0B             # load 00 into A
+  STORE A 08            # store 00 into 08
+  LOAD A 00             # load PC into A
+  INCA A                # increment
+  STORE A 00            # store new PC in 00
+  DEREF A               # deref register, now has instruction
+  STORE A 90            # write inst to address 90
+  LOAD B 06             # load last inst of program
+  BREQ end              # if last inst, end program
 GOTO_IDLE
 
 end:
-  LOAD A 0C             // load FF into A
-  STORE 0D A            // store FF into 0D
+  LOAD A 0C             # load FF into A
+  STORE A 0D            # store FF into 0D
 GOTO_IDLE
