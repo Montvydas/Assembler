90d8bdf3f9f8a894d6d1db71ac91c98b62a3b52c
Theo Scott <tmscott10@hotmail.co.uk>
2016-03-29 04:25:38 +0100

    working simulation for blue car

--- final/mouseDemoProcessor/ProcessorBackup.srcs/sources_1/imports/DSL4/IRTransmitterWrapper.v
+++ final/mouseDemoProcessor/ProcessorBackup.srcs/sources_1/imports/DSL4/IRTransmitterWrapper.v
@@ -47,11 +47,9 @@ module IRTransmitterWrapper(
             CAR_SELECT <= 4'b0000;
         end
         else if(BUS_ADDR == BASE_ADDR) begin
+            CAR_SELECT <= 4'b0001; //hard code to BLU
             COMMAND <= {BUS_DATA[0],BUS_DATA[1],BUS_DATA[2],BUS_DATA[3]}; //need to reverse command digits
         end 
-        else if (BUS_ADDR == BASE_ADDR + 1 ) begin
-        	CAR_SELECT <= BUS_DATA[3:0]; //hard code to BLU car for now
-        end
     end
     
     
@@ -60,22 +58,22 @@ module IRTransmitterWrapper(
     //generic counter configured to trigger packet sending for blue-coded car at 10Hz
     wire SEND_PACKET_BLU;
     GenericCounter #(.COUNTER_WIDTH(24), .COUNTER_MAX(10000000)) 
-        TEN_CLK_COUNT_BLU(.CLK(CLK), .RESET(RESET), .ENABLE_IN(CAR_SELECT[0]), .TRIGG_OUT(SEND_PACKET_BLU), .COUNT());
+        TEN_CLK_COUNT_BLU(.CLK(CLK), .RESET(RESET), .ENABLE_IN(1), .TRIGG_OUT(SEND_PACKET_BLU), .COUNT());
    
     //generic counter configured to trigger packet sending for yellow-coded car at 10Hz
-    wire SEND_PACKET_YEL;
-    GenericCounter #(.COUNTER_WIDTH(24), .COUNTER_MAX(10000000)) 
-        TEN_CLK_COUNT_YEL(.CLK(CLK), .RESET(RESET), .ENABLE_IN(CAR_SELECT[1]), .TRIGG_OUT(SEND_PACKET_YEL), .COUNT());
+//    wire SEND_PACKET_YEL;
+//    GenericCounter #(.COUNTER_WIDTH(24), .COUNTER_MAX(10000000)) 
+//        TEN_CLK_COUNT_YEL(.CLK(CLK), .RESET(RESET), .ENABLE_IN(CAR_SELECT[1]), .TRIGG_OUT(SEND_PACKET_YEL), .COUNT());
    
-    //generic counter configured to trigger packet sending for green-coded car at 10Hz
-    wire SEND_PACKET_GRN;
-    GenericCounter #(.COUNTER_WIDTH(24), .COUNTER_MAX(10000000)) 
-        TEN_CLK_COUNT_GRN(.CLK(CLK), .RESET(RESET), .ENABLE_IN(CAR_SELECT[2]), .TRIGG_OUT(SEND_PACKET_GRN), .COUNT());
+//    //generic counter configured to trigger packet sending for green-coded car at 10Hz
+//    wire SEND_PACKET_GRN;
+//    GenericCounter #(.COUNTER_WIDTH(24), .COUNTER_MAX(10000000)) 
+//        TEN_CLK_COUNT_GRN(.CLK(CLK), .RESET(RESET), .ENABLE_IN(CAR_SELECT[2]), .TRIGG_OUT(SEND_PACKET_GRN), .COUNT());
    
-    //generic counter configured to trigger packet sending for red-coded car at 10Hz
-    wire SEND_PACKET_RED;
-    GenericCounter #(.COUNTER_WIDTH(24), .COUNTER_MAX(10000000)) 
-        TEN_CLK_COUNT_RED(.CLK(CLK), .RESET(RESET), .ENABLE_IN(CAR_SELECT[3]), .TRIGG_OUT(SEND_PACKET_RED), .COUNT());
+//    //generic counter configured to trigger packet sending for red-coded car at 10Hz
+//    wire SEND_PACKET_RED;
+//    GenericCounter #(.COUNTER_WIDTH(24), .COUNTER_MAX(10000000)) 
+//        TEN_CLK_COUNT_RED(.CLK(CLK), .RESET(RESET), .ENABLE_IN(CAR_SELECT[3]), .TRIGG_OUT(SEND_PACKET_RED), .COUNT());
 
  //Instantiations of each colour code of car
     
@@ -84,20 +82,20 @@ module IRTransmitterWrapper(
     IRTransmitterSM #(.StartBurstSize(191), .GapSize(25), .CarSelectBurstSize(47), .AssertBurstSize(47), .DeassertBurstSize(22), .MaxCount(1389))
         BlueCar(.CLK(CLK), .RESET(RESET), .COMMAND(COMMAND), .SEND_PACKET(SEND_PACKET_BLU), .IR_LED(IR_LED_BLU));
         
-    //instantiates module for yellow coded car
-    wire IR_LED_YEL;
-    IRTransmitterSM #(.StartBurstSize(88), .GapSize(40), .CarSelectBurstSize(22), .AssertBurstSize(44), .DeassertBurstSize(22), .MaxCount(1250))
-        YellowCar(.CLK(CLK), .RESET(RESET), .COMMAND(COMMAND), .SEND_PACKET(SEND_PACKET_YEL), .IR_LED(IR_LED_YEL));
+//    //instantiates module for yellow coded car
+//    wire IR_LED_YEL;
+//    IRTransmitterSM #(.StartBurstSize(88), .GapSize(40), .CarSelectBurstSize(22), .AssertBurstSize(44), .DeassertBurstSize(22), .MaxCount(1250))
+//        YellowCar(.CLK(CLK), .RESET(RESET), .COMMAND(COMMAND), .SEND_PACKET(SEND_PACKET_YEL), .IR_LED(IR_LED_YEL));
         
-    //instantiates module for green coded car
-    wire IR_LED_GRN;
-    IRTransmitterSM #(.StartBurstSize(88), .GapSize(40), .CarSelectBurstSize(44), .AssertBurstSize(44), .DeassertBurstSize(22), .MaxCount(1333))
-        GreenCar(.CLK(CLK), .RESET(RESET), .COMMAND(COMMAND), .SEND_PACKET(SEND_PACKET_GRN), .IR_LED(IR_LED_GRN));
+//    //instantiates module for green coded car
+//    wire IR_LED_GRN;
+//    IRTransmitterSM #(.StartBurstSize(88), .GapSize(40), .CarSelectBurstSize(44), .AssertBurstSize(44), .DeassertBurstSize(22), .MaxCount(1333))
+//        GreenCar(.CLK(CLK), .RESET(RESET), .COMMAND(COMMAND), .SEND_PACKET(SEND_PACKET_GRN), .IR_LED(IR_LED_GRN));
         
-    //instantiates module for red coded car
-    wire IR_LED_RED;
-    IRTransmitterSM #(.StartBurstSize(192), .GapSize(24), .CarSelectBurstSize(24), .AssertBurstSize(48), .DeassertBurstSize(24), .MaxCount(1389))
-        RedCar(.CLK(CLK), .RESET(RESET), .COMMAND(COMMAND), .SEND_PACKET(SEND_PACKET_RED), .IR_LED(IR_LED_RED));
+//    //instantiates module for red coded car
+//    wire IR_LED_RED;
+//    IRTransmitterSM #(.StartBurstSize(192), .GapSize(24), .CarSelectBurstSize(24), .AssertBurstSize(48), .DeassertBurstSize(24), .MaxCount(1389))
+//        RedCar(.CLK(CLK), .RESET(RESET), .COMMAND(COMMAND), .SEND_PACKET(SEND_PACKET_RED), .IR_LED(IR_LED_RED));
     
     
     
@@ -105,21 +103,21 @@ module IRTransmitterWrapper(
 //When you uncomment this, set IR_LED to a reg
     // sets IR_LED to colour code wire based on input switches
     always@(CLK) begin
-        if(CAR_SELECT[0]) begin
+//        if(CAR_SELECT[0]) begin
             IR_LED <= IR_LED_BLU;
-        end
-        else if(CAR_SELECT[1]) begin
-            IR_LED <= IR_LED_YEL;
-        end
-        else if(CAR_SELECT[2]) begin
-            IR_LED <= IR_LED_GRN;
-        end
-        else if(CAR_SELECT[3]) begin
-            IR_LED <= IR_LED_RED;
-        end
-        else begin
-            IR_LED <= 0;
-        end
+//        end
+//        else if(CAR_SELECT[1]) begin
+//            IR_LED <= IR_LED_YEL;
+//        end
+//        else if(CAR_SELECT[2]) begin
+//            IR_LED <= IR_LED_GRN;
+//        end
+//        else if(CAR_SELECT[3]) begin
+//            IR_LED <= IR_LED_RED;
+//        end
+//        else begin
+//            IR_LED <= 0;
+//        end
     end
     
 endmodule
